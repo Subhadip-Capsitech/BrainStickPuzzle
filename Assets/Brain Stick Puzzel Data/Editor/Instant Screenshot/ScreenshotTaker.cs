342a1847def21eacb4d769923ec18063f85b0aee
Subhadip Manna <subhadip.manna@capsitech.com>
2025-11-14 12:28:04 +0530

    Update

--- Assets/Brain Stick Puzzel Data/Editor/Instant Screenshot/ScreenshotTaker.cs
+++ Assets/Brain Stick Puzzel Data/Editor/Instant Screenshot/ScreenshotTaker.cs
@@ -11,26 +11,26 @@ public class Screenshot : EditorWindow
     int resWidth;
     int resHeight;
 
-	public Camera myCamera;
-	int scale = 1;
+    public Camera myCamera;
+    int scale = 1;
 
-	string path = "";
-	RenderTexture renderTexture;
+    string path = "";
+    RenderTexture renderTexture;
 
-	bool isTransparent = false;
+    bool isTransparent = false;
 
-	// Add menu item named "My Window" to the Window menu
-	[MenuItem("Tools/TheGameWise/Instant High-Res Screenshot")]
-	public static void ShowWindow()
-	{
-		//Show existing window instance. If one doesn't exist, make one.
-		EditorWindow editorWindow = EditorWindow.GetWindow(typeof(Screenshot));
-		editorWindow.autoRepaintOnSceneChange = true;
-		editorWindow.Show();
-		editorWindow.title = "Screenshot";
-	}
 
-	float lastTime;
+    [MenuItem("Tools/TheGameWise/Instant High-Res Screenshot")]
+    public static void ShowWindow()
+    {
+        //Show existing window instance. If one doesn't exist, make one.
+        EditorWindow editorWindow = EditorWindow.GetWindow(typeof(Screenshot));
+        editorWindow.autoRepaintOnSceneChange = true;
+        editorWindow.Show();
+        editorWindow.title = "Screenshot";
+    }
+
+    float lastTime;
 
     void OnEnable()
     {
@@ -38,112 +38,112 @@ public class Screenshot : EditorWindow
         resWidth = (int)Handles.GetMainGameViewSize().x;
     }
 
-	void OnGUI()
-	{
-		EditorGUILayout.LabelField ("Resolution", EditorStyles.boldLabel);
-		resWidth = EditorGUILayout.IntField ("Width", resWidth);
-		resHeight = EditorGUILayout.IntField ("Height", resHeight);
+    void OnGUI()
+    {
+        EditorGUILayout.LabelField("Resolution", EditorStyles.boldLabel);
+        resWidth = EditorGUILayout.IntField("Width", resWidth);
+        resHeight = EditorGUILayout.IntField("Height", resHeight);
 
-		EditorGUILayout.Space();
+        EditorGUILayout.Space();
 
-		scale = EditorGUILayout.IntSlider ("Scale", scale, 1, 15);
+        scale = EditorGUILayout.IntSlider("Scale", scale, 1, 15);
 
-		EditorGUILayout.HelpBox("The default mode of screenshot is crop - so choose a proper width and height. The scale is a factor " +
-			"to multiply or enlarge the renders without loosing quality.",MessageType.None);
-
-		
-		EditorGUILayout.Space();
-		
-		
-		GUILayout.Label ("Save Path", EditorStyles.boldLabel);
-
-		EditorGUILayout.BeginHorizontal();
-		EditorGUILayout.TextField(path,GUILayout.ExpandWidth(false));
-		if(GUILayout.Button("Browse",GUILayout.ExpandWidth(false)))
-			path = EditorUtility.SaveFolderPanel("Path to Save Images",path,Application.dataPath);
-
-		EditorGUILayout.EndHorizontal();
-
-		EditorGUILayout.HelpBox("Choose the folder in which to save the screenshots ",MessageType.None);
-		EditorGUILayout.Space();
+        EditorGUILayout.HelpBox("The default mode of screenshot is crop - so choose a proper width and height. The scale is a factor " +
+            "to multiply or enlarge the renders without loosing quality.", MessageType.None);
 
 
+        EditorGUILayout.Space();
 
-		//isTransparent = EditorGUILayout.Toggle(isTransparent,"Transparent Background");
+
+        GUILayout.Label("Save Path", EditorStyles.boldLabel);
+
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.TextField(path, GUILayout.ExpandWidth(false));
+        if (GUILayout.Button("Browse", GUILayout.ExpandWidth(false)))
+            path = EditorUtility.SaveFolderPanel("Path to Save Images", path, Application.dataPath);
+
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.HelpBox("Choose the folder in which to save the screenshots ", MessageType.None);
+        EditorGUILayout.Space();
 
 
 
-		GUILayout.Label ("Select Camera", EditorStyles.boldLabel);
+        //isTransparent = EditorGUILayout.Toggle(isTransparent,"Transparent Background");
 
 
-		myCamera = EditorGUILayout.ObjectField(myCamera, typeof(Camera), true,null) as Camera;
+
+        GUILayout.Label("Select Camera", EditorStyles.boldLabel);
 
 
-		isTransparent = EditorGUILayout.Toggle("Transparent Background", isTransparent);
+        myCamera = EditorGUILayout.ObjectField(myCamera, typeof(Camera), true, null) as Camera;
 
 
-		EditorGUILayout.HelpBox("Choose the camera of which to capture the render. You can make the background transparent using the transparency option.",MessageType.None);
-
-		EditorGUILayout.Space();
-		EditorGUILayout.BeginVertical();
-		EditorGUILayout.LabelField ("Default Options", EditorStyles.boldLabel);
+        isTransparent = EditorGUILayout.Toggle("Transparent Background", isTransparent);
 
 
-		if(GUILayout.Button("Set To Screen Size"))
-		{
-			resHeight = (int)Handles.GetMainGameViewSize().y;
-			resWidth = (int)Handles.GetMainGameViewSize().x;
-		
-		}
+        EditorGUILayout.HelpBox("Choose the camera of which to capture the render. You can make the background transparent using the transparency option.", MessageType.None);
+
+        EditorGUILayout.Space();
+        EditorGUILayout.BeginVertical();
+        EditorGUILayout.LabelField("Default Options", EditorStyles.boldLabel);
 
 
-		EditorGUILayout.EndVertical();
+        if (GUILayout.Button("Set To Screen Size"))
+        {
+            resHeight = (int)Handles.GetMainGameViewSize().y;
+            resWidth = (int)Handles.GetMainGameViewSize().x;
 
-		EditorGUILayout.Space();
-		EditorGUILayout.LabelField ("Screenshot will be taken at " + resWidth*scale + " x " + resHeight*scale + " px", EditorStyles.boldLabel);
-
-		if(GUILayout.Button("Take Screenshot",GUILayout.MinHeight(60)))
-		{
-			if(path == "")
-			{
-				path = EditorUtility.SaveFolderPanel("Path to Save Images",path,Application.dataPath);
-				Debug.Log("Path Set");
-				TakeHiResShot();
-			}
-			else
-			{
-				TakeHiResShot();
-			}
-		}
-
-		EditorGUILayout.Space();
-		EditorGUILayout.BeginHorizontal();
-
-		if(GUILayout.Button("Open Last Screenshot",GUILayout.MaxWidth(160),GUILayout.MinHeight(40)))
-		{
-			if(lastScreenshot != "")
-			{
-				Application.OpenURL("file://" + lastScreenshot);
-				Debug.Log("Opening File " + lastScreenshot);
-			}
-		}
-
-		if(GUILayout.Button("Open Folder",GUILayout.MaxWidth(100),GUILayout.MinHeight(40)))
-		{
-
-			Application.OpenURL("file://" + path);
-		}
-
-		if(GUILayout.Button("More Assets",GUILayout.MaxWidth(100),GUILayout.MinHeight(40)))
-		{
-			Application.OpenURL("https://www.assetstore.unity3d.com/en/#!/publisher/5951");
-		}
-
-		EditorGUILayout.EndHorizontal();
+        }
 
 
-		if (takeHiResShot) 
-		{
+        EditorGUILayout.EndVertical();
+
+        EditorGUILayout.Space();
+        EditorGUILayout.LabelField("Screenshot will be taken at " + resWidth * scale + " x " + resHeight * scale + " px", EditorStyles.boldLabel);
+
+        if (GUILayout.Button("Take Screenshot", GUILayout.MinHeight(60)))
+        {
+            if (path == "")
+            {
+                path = EditorUtility.SaveFolderPanel("Path to Save Images", path, Application.dataPath);
+                Debug.Log("Path Set");
+                TakeHiResShot();
+            }
+            else
+            {
+                TakeHiResShot();
+            }
+        }
+
+        EditorGUILayout.Space();
+        EditorGUILayout.BeginHorizontal();
+
+        if (GUILayout.Button("Open Last Screenshot", GUILayout.MaxWidth(160), GUILayout.MinHeight(40)))
+        {
+            if (lastScreenshot != "")
+            {
+                Application.OpenURL("file://" + lastScreenshot);
+                Debug.Log("Opening File " + lastScreenshot);
+            }
+        }
+
+        if (GUILayout.Button("Open Folder", GUILayout.MaxWidth(100), GUILayout.MinHeight(40)))
+        {
+
+            Application.OpenURL("file://" + path);
+        }
+
+        if (GUILayout.Button("More Assets", GUILayout.MaxWidth(100), GUILayout.MinHeight(40)))
+        {
+            Application.OpenURL("https://www.assetstore.unity3d.com/en/#!/publisher/5951");
+        }
+
+        EditorGUILayout.EndHorizontal();
+
+
+        if (takeHiResShot)
+        {
             int resWidthN = resWidth * scale;
             int resHeightN = resHeight * scale;
             string filename = ScreenShotName(resWidthN, resHeightN);
@@ -179,36 +179,39 @@ public class Screenshot : EditorWindow
             takeHiResShot = false;
         }
 
-		EditorGUILayout.HelpBox("In case of any error, make sure you have Unity Pro as the plugin requires Unity Pro to work.",MessageType.Info);
+        EditorGUILayout.HelpBox("In case of any error, make sure you have Unity Pro as the plugin requires Unity Pro to work.", MessageType.Info);
 
 
-	}
-
-
-	
-	private bool takeHiResShot = false;
-	public string lastScreenshot = "";
-	
-		
-	public string ScreenShotName(int width, int height) {
-
-		string strPath="";
-
-		strPath = string.Format("{0}/screen_{1}x{2}_{3}.png", 
-		                     path, 
-		                     width, height, 
-		                               System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss"));
-		lastScreenshot = strPath;
-	
-		return strPath;
-	}
+    }
 
 
 
-	public void TakeHiResShot() {
-		Debug.Log("Taking Screenshot");
-		takeHiResShot = true;
-	}
+    private bool takeHiResShot = false;
+    public string lastScreenshot = "";
+
+
+    public string ScreenShotName(int width, int height)
+    {
+
+        string strPath = "";
+
+        strPath = string.Format("{0}/screen_{1}x{2}_{3}.png",
+                             path,
+                             width, height,
+                                       System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss"));
+        lastScreenshot = strPath;
+
+        return strPath;
+    }
+
+
+
+    public void TakeHiResShot()
+    {
+        Debug.Log("Taking Screenshot");
+        takeHiResShot = true;
+    }
 
 }
 
+    
